97a0e02d90cc47be4bd0955ce20161a48393e8e5
ty-codes <titoadeoye00@gmail.com>
2024-02-23 06:03:52 -0800

    refactor: create assets directory file

---
+++ src/assets/index.js
@@ -0,0 +1,30 @@
+"use client";
+
+// // import { lazy } from 'react';
+
+// // const MeasurementIcon = lazy(() => import('@/svg/measurement.svg'));
+// // const CatalogueIcon = lazy(() => import('@/svg/catalogue.svg'));
+// // const UploadIcon = lazy(() => import('@/svg/upload.svg'));
+// // const FolderIcon = lazy(() => import('@/svg/folder.svg'));
+
+import MeasurementIcon from "@/svg/measurement.svg";
+import CatalogueIcon from "@/svg/catalogue.svg";
+import UploadIcon from "@/svg/upload.svg";
+import FolderIcon from "@/svg/folder.svg";
+import TwitterIcon from "@/svg/twitter.svg";
+import FacebookIcon from "@/svg/facebook.svg";
+import InstagramIcon from "@/svg/instagram.svg";
+import LinkedinIcon from "@/svg/linkedin.svg";
+import EmailIcon from "@/svg/email.svg";
+
+export {
+  MeasurementIcon,
+  CatalogueIcon,
+  UploadIcon,
+  FolderIcon,
+  TwitterIcon,
+  FacebookIcon,
+  InstagramIcon,
+  LinkedinIcon,
+  EmailIcon
+};
